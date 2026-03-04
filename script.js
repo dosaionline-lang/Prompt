fc372dc5b7ec9f3696706ae07bf07c4b0acf2df3
dosaionline-lang <dosai.online@gmail.com>
2026-03-04 20:42:38 +0500

    Change display style from 'block' to 'flex'

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 function goToPage2()  {
   document.getElementById("page1").style.display = "none";
-  document.getElementById("page2").style.display = "block";
+  document.getElementById("page2").style.display = "flex";
 }
 function goToPage3()  {
   document.getElementById("page2").style.display = "none";
-  document.getElementById("page3").style.display = "block";
+  document.getElementById("page3").style.display = "flex";
 }
 function backFromPage3()  {
   document.getElementById("page3").style.display = "none";
-  document.getElementById("page2").style.display = "block";
+  document.getElementById("page2").style.display = "flex";
 }
 function goToPage4()  {
   document.getElementById("page2").style.display = "none";
-  document.getElementById("page4").style.display = "block";
+  document.getElementById("page4").style.display = "flex";
 }
 function backFromPage4()  {
   document.getElementById("page4").style.display = "none";
-  document.getElementById("page2").style.display = "block";
+  document.getElementById("page2").style.display = "flex";
 }
 function goToPage5()  {
   document.getElementById("page2").style.display = "none";
-  document.getElementById("page5").style.display = "block";
+  document.getElementById("page5").style.display = "flex";
 }
 function backFromPage5()  {
   document.getElementById("page5").style.display = "none";
-  document.getElementById("page2").style.display = "block";
+  document.getElementById("page2").style.display = "flex";
 }

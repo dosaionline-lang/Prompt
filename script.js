f7c368e5e80cbe237fa6500cdf67bac26977d3a2
dosaionline-lang <dosai.online@gmail.com>
2026-03-04 20:17:40 +0500

    Add navigation functions for multiple pages

---
+++ script.js
@@ -0,0 +1,28 @@
+function goToPage2()  {
+  document.getElementById("page1").style.display = "none";
+  document.getElementById("page2").style.display = "block";
+}
+function goToPage3()  {
+  document.getElementById("page2").style.display = "none";
+  document.getElementById("page3").style.display = "block";
+}
+function backFromPage3()  {
+  document.getElementById("page3").style.display = "none";
+  document.getElementById("page2").style.display = "block";
+}
+function goToPage4()  {
+  document.getElementById("page2").style.display = "none";
+  document.getElementById("page4").style.display = "block";
+}
+function backFromPage4()  {
+  document.getElementById("page4").style.display = "none";
+  document.getElementById("page2").style.display = "block";
+}
+function goToPage5()  {
+  document.getElementById("page2").style.display = "none";
+  document.getElementById("page5").style.display = "block";
+}
+function backFromPage5()  {
+  document.getElementById("page5").style.display = "none";
+  document.getElementById("page2").style.display = "block";
+}
